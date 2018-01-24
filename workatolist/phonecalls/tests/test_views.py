8b7e7b823fa7547700e8e06d42cd29c8c5fa7527
Giovani <sousa_giovani@hotmail.com>
2018-01-24 15:48:57 -0200

    Add CallView tests

---
+++ workatolist/phonecalls/tests/test_views.py
@@ -0,0 +1,46 @@
+from datetime import datetime, timedelta
+
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from mixer.backend.django import mixer
+
+from workatolist.phonecalls.models import Call
+
+
+class CallViewTest(TestCase):
+
+    def setUp(self):
+        self.url = reverse('api:call')
+        config = {'id': (n for n in (42, 43)),
+                  'started_at': (start for start in (datetime.now()-timedelta(days=3), None)),
+                  'finished_at': (finish for finish in (None, datetime.now())),
+                  'source': (phone for phone in ('1140228922', None)),
+                  'destination': (phone for phone in ('4490456899', None))}
+        mixer.cycle(2).blend(Call, **config)
+
+    def test_create_type_start(self):
+        data = {'id': 2, 'type': 'start', 'timestamp': '1516757933', 'call_id':1,
+                'source': '1159886999', 'destination':'11998866991'}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Call.objects.filter(id=1).exists())
+
+    def test_create_type_end(self):
+        data = {'id': 1, 'type': 'end', 'timestamp': '1516757933', 'call_id':2}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Call.objects.filter(id=2).exists())
+
+    def test_update_type_start(self):
+        data = {'id': 36, 'type': 'start', 'timestamp': '1516757933', 'call_id':43,
+                'source': '1159886999', 'destination':'11998866991'}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Call.objects.get(id=43).price)
+
+    def test_update_type_end(self):
+        data = {'id': 25, 'type': 'end', 'timestamp': '1516757933', 'call_id':42}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
