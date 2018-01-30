553a9e7068d684f547cded139a9ff83cd2c3df04
Giovani <sousa_giovani@hotmail.com>
2018-01-30 11:05:58 -0200

    Configure to serve static files

--- workatolist/settings.py
+++ workatolist/settings.py
@@ -126,4 +126,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
