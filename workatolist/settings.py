ad0f87cfeb267a50627d0bc2b11a9c7583a705b7
Giovani <sousa_giovani@hotmail.com>
2018-01-30 00:54:45 -0200

    Fix static configuration

--- workatolist/settings.py
+++ workatolist/settings.py
@@ -126,12 +126,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
